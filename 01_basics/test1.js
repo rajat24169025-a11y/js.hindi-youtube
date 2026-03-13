5b1cc81c58bde3158b4e1302dbe975b58188333b
rajat tiwari <rajat24169025@akgec.ac.in>
2026-03-13 12:43:48 +0000

    new h tuu

---
+++ 01_basics/test1.js
@@ -0,0 +1,37 @@
+/*      // javascript ki  journey by chai
+const accountid = 2983
+let accountemail = "143rajattiwari@akgcc.in"
+let accountdetails;    //iski value undefine hogi 
+var accountpassword = "12322"   //var ko use nhi krte kyu ki
+//  block or functional scope ki dikkat aati h yaha 
+
+accountemail="rajat@akgec.in"
+// accountid = 9346cle    ye krna galat h kyu ki const ki value chnage nhi hoti 
+accountpassword = "43859"
+console.table([accountemail,accountid,accountpassword]) //ek table ki form me print hoga 
+*/
+
+
+//"use strict"; //treat all js code as newer version 
+  // alert(3+3) we are using node js, not browser 
+
+  //  data types
+  //number => 2 to power 53 or bigint ye or bade number ke liye h 
+  //string => ""
+  //boolean => true/false
+  //null => standalone value
+  //undefined => 
+  //symbol => unique 
+  //object
+
+
+  let age = 299
+  let name = "219";
+  console.log(typeof age )
+  console.log(typeof null) 
+  
+  let valueInNumber = Number(name);  //ye krne se hm kisi ka data type change kr skte h
+  console.log(typeof valueInNumber)   //but ye error bhi h agr  name="387jj" ho fir ??
+  console.log(valueInNumber)
+
+  
